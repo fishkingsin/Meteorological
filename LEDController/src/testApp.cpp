fdbccbae615ba228d061335f2f03bc985352a4f0
fishkingsin <fishkingsin@gmail.com>
2012-11-18 01:38:12 +0800

    led controller update

--- LEDController/src/testApp.cpp
+++ LEDController/src/testApp.cpp
@@ -2,17 +2,104 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
+    ofSetFrameRate(60);
+    ofSetBackgroundColor(0);
+    LEDs.assign(NUM_LED*NUM_LED, LED());
+    for(int y = 0 ; y < NUM_LED ; y++)
+    {
+        for(int x = 0 ; x < NUM_LED ; x++)
+        {
+            int i = x+y*NUM_LED;
+            LEDs[i].set(x,y);
+            
+        }
+        
+    }
+    
+    vidGrabber.setVerbose(true);
+    vidGrabber.initGrabber(320,240);
+    colorImg.allocate(320,240);
+	grayImage.allocate(320,240);
+	grayBg.allocate(320,240);
+	grayDiff.allocate(320,240);
+    
+	bLearnBakground = true;
+	threshold = 80;
+
+    
+    ofxXmlSettings settings;
+    settings.loadFile("config.xml");
+    
+    gui.setup(settings.getValue("GUI_WIDTH",1280),settings.getValue("GUI_HEIGHT",768));
+	gui.loadFont(settings.getValue("FONT","MONACO.TTF"), 8);
+	gui.ofxControlPanel::addPanel("General", 1);
+	gui.setWhichPanel("Settings");
+    gui.addSlider("LED_SIZE", 1, 1, 100,true);
+	gui.addSlider("PADDING", 1, 1, 100,true );
+    gui.addSlider("PADDING", 1, 1, 100,true );
+    gui.addToggle("LEARN_BACKGROUND", bLearnBakground );
+    gui.addSlider("THRESHOLD", threshold,1,255,true );
+    
+	//  -- this gives you back an ofEvent for all events in this control panel object
+	ofAddListener(gui.guiEvent, this, &testApp::eventsIn);
+	gui.setupEvents();
+	gui.enableEvents();
+	gui.loadSettings("control_settings.xml");
+    
+
 
 }
+void testApp::eventsIn(guiCallbackData & data){
+	if( data.isElement( "LED_SIZE" ) )
+	{
+        size = data.getInt(0);
+    }
+    else if( data.isElement( "PADDING" ) )
+	{
+        padding = data.getInt(0);
+    }
+    else if( data.isElement( "LEARN_BACKGROUND" ) )
+	{
 
+        gui.setValueB("LEARN_BACKGROUND", false);
+        grayBg = grayImage;
+    }
+    else if( data.isElement( "THRESHOLD" ) )
+	{
+        threshold = data.getInt(0);
+       
+    }
+
+}
 //--------------------------------------------------------------
 void testApp::update(){
+    bool bNewFrame = false;
+    
+    vidGrabber.update();
+    bNewFrame = vidGrabber.isFrameNew();
+    
+	if (bNewFrame){
+        
+        colorImg.setFromPixels(vidGrabber.getPixels(), 320,240);
 
+        
+        grayImage = colorImg;
+		// take the abs value of the difference between background and incoming and then threshold:
+		grayDiff.absDiff(grayBg, grayImage);
+		grayDiff.threshold(threshold);
+	}
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
+    ofBackground(0);
+    ofSetColor(255);
+    vector<LED>::iterator it;
+    for(it = LEDs.begin() ; it!=LEDs.end() ;it++)
+    {
+        ofCircle(20+it->x*(size+padding), 20+it->y*(size+padding), size*0.5);
+    }
+    
 }
 
 //--------------------------------------------------------------
--- LEDController/src/testApp.h
+++ LEDController/src/testApp.h
@@ -1,7 +1,15 @@
 #pragma once
 
 #include "ofMain.h"
-
+#include "ofxAutoControlPanel.h"
+#include "ofxCvMain.h"
+#define NUM_LED 50
+class LED : public ofVec2f
+{
+public:
+    
+    int brightness;
+};
 class testApp : public ofBaseApp{
 
 	public:
@@ -19,4 +27,17 @@ class testApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+    vector <LED> LEDs;
+    ofxAutoControlPanel gui;
+    void eventsIn(guiCallbackData & data);
+    int size,padding;
+    ofVideoGrabber 		vidGrabber;
+    ofxCvColorImage			colorImg;
+    
+    ofxCvGrayscaleImage 	grayImage;
+    ofxCvGrayscaleImage 	grayBg;
+    ofxCvGrayscaleImage 	grayDiff;
+    int 				threshold;
+    bool				bLearnBakground;
+
 };
